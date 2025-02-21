fb2c6760ad5266de4e9fdf6a9c0b41edf1cd9c0e
jolene <jolenechong7@gmail.com>
2025-02-22 00:19:01 +0800

    feat: added click count functionality

--- server/api/shorten/access.js
+++ server/api/shorten/access.js
@@ -15,6 +15,12 @@ async function access(req, res) {
 
         if (result.rows.length == 0) { return res.status(404).end("URL not found."); }
 
+        // increment click_count
+        await pool.query(
+            `UPDATE urls SET click_count = click_count + 1 WHERE short_url = $1`,
+            [url]
+        );
+
         return res.json({
             long_url: result.rows[0].long_url
         }).end();
--- server/api/shorten/index.js
+++ server/api/shorten/index.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import shorten from "./shorten.js";
 import access from "./access.js";
+import get from "./get.js";
 import verify from "../auth/verify.js";
 
 const router = Router();
 
 router.post("/shorten", verify, shorten);
 router.post("/access", verify, access);
+router.get("/all", get);
 
 export default router;
