4ac13c9825c364582382b17fd84d8ec6a06ee1df
jolene <jolenechong7@gmail.com>
2025-02-23 23:50:45 +0800

    feat: ensure that short path URL is unique efficiently

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # UrlShortener
 
+UrlShortener is a PERN Full-Stack Web Appplication which allows users to easily shorten long URLs and access them via a short, unique URL. 
+
+
 Run the program with `npm run start`
 
 Run load tests with `k6 run ./tests/load_test.js`
--- server/api/shorten/shorten.js
+++ server/api/shorten/shorten.js
@@ -4,9 +4,6 @@ async function shorten(req, res) {
 
     const { url } = req.body;
 
-    // generate a 5 character path of alphanumerics for the shortened URL
-    const path = generateRandomString();
-
     // check if auth exists
     if (!req.body.auth || !req.body.auth.ui || !req.body.auth.ui.id) {
         return res.status(401).end("Unauthorized");
@@ -14,38 +11,60 @@ async function shorten(req, res) {
 
     const user_id = req.body.auth.ui.id;
     const created_at = new Date();
-    const short_url = `http://localhost:3001/access/${path}`; // hard save domain for now
+    // const short_url = `http://localhost:3001/access/${path}`; // hard save domain for now
 
-    // save the original URL and the shortened URL to the postgres database
+    // generate a 5 character path of alphanumerics for the shortened URL
+    // check that path hasn't been used before in db
+    let result;
     try {
-        // urls table has user_id, long_url, short_url, click_count and created_at columns
-        const result = await pool.query(
-            `INSERT INTO urls (user_id, long_url, short_url, click_count, created_at) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
-            [user_id, url, short_url, 0, created_at]
+        result = await pool.query(
+            `SELECT short_url FROM urls`
         );
-        console.log(result.rows[0]);
-
-        return res.json({
-            long_url: url,
-            short_url: short_url
-        }).end();
-
     } catch (err) {
         console.error(err);
         return res.status(500).end("Internal Error Occurred.");
     }
+
+    const existingUrls = new Set(result.rows.map(row => row.short_url));
+
+    let short_url;
+    let path;
+    do {
+        path = generateRandomString();
+        short_url = `http://localhost:3001/access/${path}`;
+
+        // save the original URL and the shortened URL to the postgres database
+        try {
+            // urls table has user_id, long_url, short_url, click_count and created_at columns
+            const result = await pool.query(
+                `INSERT INTO urls (user_id, long_url, short_url, click_count, created_at) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+                [user_id, url, short_url, 0, created_at]
+            );
+            console.log(result.rows[0]);
+
+            return res.json({
+                long_url: url,
+                short_url: short_url
+            }).end();
+
+        } catch (err) {
+            console.error(err);
+            return res.status(500).end("Internal Error Occurred.");
+        }
+    } while (existingUrls.has(short_url));
+
 }
 
 function generateRandomString(length = 5) {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let result = '';
-    
+
     for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * characters.length);
-      result += characters.charAt(randomIndex);
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters.charAt(randomIndex);
     }
-    
+
     return result;
-  }
+}
 
 export default shorten;
